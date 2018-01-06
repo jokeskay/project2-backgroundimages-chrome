88394a1d8e9d1d651df1697016dceb97237498c5
jokeskay <34804182+jokeskay@users.noreply.github.com>
2018-01-05 20:21:52 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,6 @@
 //Copyright (c) 2017 All rights reserved
 //Found in the LICENSE file
 
-function getCurrentTabUrl(callback) {
-   var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-  
-  chrome.tabs.query(queryInfo, (tabs) => {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-}
-
 function click(e) {
    chrome.tabs.executeScript(null,
       {code:"document.body.style.backgroundImage='url(" + images[e.target.id] 
@@ -29,25 +14,6 @@ var images = {
    yellow background = 'http://i64.tinypic.com/t6fec4.jpg'
    red laser = 'http://i64.tinypic.com/2zgda1v.jpg'
 
-function changeBackground(images) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
-function getSavedBackgroundColor(url, callback) {
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  chrome.storage.sync.set(items);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   getCurrentTabUrl((url) => {
     var dropdown = document.getElementById('dropdown');
@@ -57,9 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
         dropdown.value = savedColor;
       }
     });
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });
+    
   });
 });
