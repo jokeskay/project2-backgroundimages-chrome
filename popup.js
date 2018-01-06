f2e463c9fe09b9bb01cf7a465ee720ab41fa864a
jokeskay <34804182+jokeskay@users.noreply.github.com>
2018-01-05 23:02:53 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -9,8 +9,8 @@ function click(e) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-   var divs=document.querySelectorAll('div');
-   for (var i=0; i<divs.length; i++) {
+   var divs = document.querySelectorAll('div');
+   for (var i = 0; i <divs.length; i++) {
       divs[i].addEventListener('click', click);
    }
 });
@@ -18,6 +18,6 @@ document.addEventListener('DOMContentLoaded', function () {
 var images = {
    staring away: 'http://i64.tinypic.com/t6fec4.jpg',
    bright lights: 'http://i66.tinypic.com/1z4ev7o.jpg',
-   yellow background: 'http://i64.tinypic.com/t6fec4.jpg',
-   red laser: 'http://i64.tinypic.com/2zgda1v.jpg'
+yellow background: 'http://i64.tinypic.com/t6fec4.jpg',
+red laser: 'http://i64.tinypic.com/2zgda1v.jpg'
 }
