371a87746a004b01d45ba4bcfe5597432142227d
jokeskay <34804182+jokeskay@users.noreply.github.com>
2018-01-05 22:00:57 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,11 +6,11 @@ function click(e) {
       {code:"document.body.style.backgroundImage='url(" + images[e.target.id] 
         + "'"});
    window.close();
-},
+}
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
    var divs=document.querySelectorAll('div');
-   for (var i=0; i<divs,length; i++) {
+   for (var i=0; i<divs.length; i++) {
       divs[i].addEventListener('click', click);
    }
 });
