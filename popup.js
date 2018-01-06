4a5963beee4ad00f437833d5aa1719e4c4efa216
jokeskay <34804182+jokeskay@users.noreply.github.com>
2018-01-05 20:50:22 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -17,6 +17,5 @@ document.addEventListener(DOMContentLoaded', function() {
    var divs=document.querySelectorAll('div');
    for (var i=0; i<divs,length; i++) {
       divs[i].addEventListener(click,click);
-   }
-});
-});
+   });
+}
