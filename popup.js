e015b4a8420e3afba5c45cbcaf7579ed4f564898
jokeskay <34804182+jokeskay@users.noreply.github.com>
2018-01-05 21:30:43 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ function click(e) {
    window.close();
 },
 
-document.addEventListener(DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
    var divs=document.querySelectorAll('div');
    for (var i=0; i<divs,length; i++) {
       divs[i].addEventListener('click', click);
