1c1829c0f8462927436ee4e6bcda9e47ccbd3b01
jokeskay <34804182+jokeskay@users.noreply.github.com>
2018-01-07 23:04:18 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 var images = {
-   staring away: 'http://i64.tinypic.com/t6fec4.jpg',
-   bright lights: 'http://i66.tinypic.com/1z4ev7o.jpg',
+staring away: 'http://i64.tinypic.com/t6fec4.jpg',
+bright lights: 'http://i66.tinypic.com/1z4ev7o.jpg',
 yellow background: 'http://i64.tinypic.com/t6fec4.jpg',
 red laser: 'http://i64.tinypic.com/2zgda1v.jpg'
 }
